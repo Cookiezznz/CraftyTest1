a37b70102fdfbd34f5c37b2cda8866c713474611
Cookiezz <Braydencooke@hotmail.com>
2021-07-21 21:07:00 +1200

    Colour Change

--- scenes/game.js
+++ scenes/game.js
@@ -2,11 +2,11 @@ Crafty.defineScene("Game", function() {
 	
 	var redSquare = Crafty.e('2D, DOM, Color, Twoway, Gravity')
 		.attr({x: 0, y: 0, w: 100, h: 100})
-		.color('#F00')
+		.color('#F69')
 		.twoway(200)
 		.gravity("solid");
 
 	var platform = Crafty.e('2D, DOM, Color, solid')
 		.attr({x: 0, y: 300, w: 1000, h: 30})
-		.color('green');
+		.color('purple');
 });
