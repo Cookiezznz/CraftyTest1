8bd6cd2b74c63636850e100a37cf1f91b4eeab74
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2021-07-21 11:45:07 +0300

    added gravity and platform

--- scenes/game.js
+++ scenes/game.js
@@ -1,7 +1,12 @@
 Crafty.defineScene("Game", function() {
 	
-	var redSquare = Crafty.e('2D, DOM, Color')
+	var redSquare = Crafty.e('2D, DOM, Color, Twoway, Gravity')
 		.attr({x: 0, y: 0, w: 100, h: 100})
-		.color('#F00');
+		.color('#F00')
+		.twoway(200)
+		.gravity("solid");
 
+	var platform = Crafty.e('2D, DOM, Color, solid')
+		.attr({x: 0, y: 300, w: 1000, h: 30})
+		.color('green');
 });
