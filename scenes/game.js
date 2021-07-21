f8c1a8b7e2c12f74f5e7865c8cfce51187b51ea6
Cookiezz <Braydencooke@hotmail.com>
2021-07-21 21:17:15 +1200

    Level0

--- scenes/game.js
+++ scenes/game.js
@@ -7,14 +7,22 @@ Crafty.defineScene("Game", function() {
 		.gravity("solid");
 
 	var platform = Crafty.e('2D, DOM, Color, solid')
-		.attr({x: 0, y: 900, w: 200, h: 30})
+		.attr({x: 0, y: 150, w: 200, h: 30})
 		.color('purple');
 
 	var platform1 = Crafty.e('2D, DOM, Color, solid')
-		.attr({x: 200, y: 300, w: 30, h: 300})
+		.attr({x: 200, y: 150, w: 30, h: 300})
 		.color('purple');
 
 	var platform2 = Crafty.e('2D, DOM, Color, solid')
-		.attr({x: 0, y: 300, w: 200, h: 30})
+		.attr({x: 200, y: 450, w: 100, h: 30})
+		.color('purple');
+
+	var platform3 = Crafty.e('2D, DOM, Color, solid')
+		.attr({x: 200, y: 450, w: 300, h: 30})
+		.color('purple');
+
+	var platform4 = Crafty.e('2D, DOM, Color, solid')
+		.attr({x: 500, y: 300, w: 300, h: 30})
 		.color('purple');
 });
