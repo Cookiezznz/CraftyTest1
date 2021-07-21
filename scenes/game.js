e5876f89f13fcb0521519bb3b7fe91cdf63392be
Cookiezz <Braydencooke@hotmail.com>
2021-07-21 20:50:21 +1200

    No changes

--- scenes/game.js
+++ scenes/game.js
@@ -1,12 +1,7 @@
 Crafty.defineScene("Game", function() {
 	
-	var redSquare = Crafty.e('2D, DOM, Color, Twoway, Gravity')
+	var redSquare = Crafty.e('2D, DOM, Color')
 		.attr({x: 0, y: 0, w: 100, h: 100})
-		.color('#F00')
-		.twoway(200)
-		.gravity("solid");
+		.color('#F00');
 
-	var platform = Crafty.e('2D, DOM, Color, solid')
-		.attr({x: 0, y: 300, w: 1000, h: 30})
-		.color('green');
 });
